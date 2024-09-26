45ea6b06ea11ca9af7f41f8218291c43c4ae415d
Leonardo Pizarro <lpizarro@Leonardos-MacBook-Pro.local>
2024-09-26 03:40:15 -0300

    Example app following the first steps guide from the official documentation site

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+  http_basic_authenticate_with name: 'tester', password: 'testing1234', except: [:index, :show]
+
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if (@post.save)
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body, :status)
+    end
+end
